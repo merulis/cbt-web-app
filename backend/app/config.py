f2409aa6829be6cc22b32ee0180742cd7a8977fe
merulis <merulis@yandex.ru>
2024-10-14 17:18:58 +0300

    fix db_url

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,4 +7,4 @@ class Settings(BaseSettings):
     db_pass = ""
     db_address_server: str = "localhost"
     db_name: str = "db_name"
-    db_url: str = f"{DB_DRIVER}://{DB_USER}:{DB_PASSSWORD}@{DB_SERVER}/{DB_NAME}"
+    db_url: str = f"{db_driver}://{db_user}:{db_pass}@{db_address_server}/{db_name}"
