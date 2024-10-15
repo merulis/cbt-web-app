964c0e08adc9b4c7efc02c12807f955430e5f190
merulis <merulis@yandex.ru>
2024-10-15 08:56:29 +0300

    rename to short name db_address_server -> db_server

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -5,9 +5,9 @@ class Settings(BaseSettings):
     db_driver: str = "postgresql+asyncpg"
     db_user: str = "postgres"
     db_pass: str = "postgres"
-    db_address_server: str = "localhost"
+    db_server: str = "localhost"
     db_name: str = "dev_db"
-    db_url: str = f"{db_driver}://{db_user}:{db_pass}@{db_address_server}/{db_name}"
+    db_url: str = f"{db_driver}://{db_user}:{db_pass}@{db_server}/{db_name}"
     db_echo: bool = True  # only for dev
 
 
