230cf98d5e71575ce077c44caf7be7748dcb5af6
merulis <merulis@yandex.ru>
2024-10-16 20:36:28 +0300

    Add repr() in base sqlmodel

--- backend/app/core/db/models/base.py
+++ backend/app/core/db/models/base.py
@@ -14,3 +14,10 @@ class Base(DeclarativeBase):
         return cls.__name__.lower()
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
+    def __str__(self):
+        cls_name = self.__class__.__name__
+        return f"{cls_name}: {str(self.__dict__)}"
+
+    def __repr__(self):
+        return str(self)
