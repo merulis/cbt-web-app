f370846dfd27037a453e7b70357802f09eeb889c
merulis <merulis@yandex.ru>
2024-10-16 12:03:18 +0300

    add model user + migration

--- backend/app/core/db/__init__.py
+++ backend/app/core/db/__init__.py
@@ -1,10 +1,12 @@
 __all__ = (
     "Base",
     "Activity",
+    "User",
     "DataBase",
     "database",
 )
 
 from .models.base import Base
-from .models.models import Activity
+from .models.activity import Activity
+from .models.user import User
 from .database import DataBase, database
