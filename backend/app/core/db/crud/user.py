f7e58c50fe813457d0d739d8559a8a918d55810f
merulis <merulis@yandex.ru>
2024-10-16 20:37:08 +0300

    Check SQLAlchemy API for data quering

---
+++ backend/app/core/db/crud/user.py
@@ -0,0 +1,78 @@
+import asyncio
+
+from datetime import datetime, timedelta
+
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db import database, User, Profile, Activity
+
+
+async def create_user(session: AsyncSession, username: str) -> User:
+    user = User(username=username)
+    session.add(user)
+    await session.commit()
+    print("user", user)
+    return user
+
+
+async def get_user_by_username(
+    session: AsyncSession,
+    username: str,
+) -> User | None:
+    stmt = select(User).where(User.username == username)
+    user: User | None = await session.scalar(stmt)
+    print(f"Found user {username}: {user}")
+    return user
+
+
+async def create_profile(
+    session: AsyncSession,
+    user_id: int,
+    fname: str | None = None,
+    lname: str | None = None,
+    bio: str | None = None,
+) -> Profile:
+    profile = Profile(user_id=user_id, first_name=fname, last_name=lname, bio=bio)
+    session.add(profile)
+    await session.commit()
+
+    return profile
+
+
+async def show_users_and_profile(session: AsyncSession):
+    stmt = select(User).options(joinedload(User.profile)).order_by(User.id)
+    users = await session.scalars(stmt)
+
+    for user in users:
+        print(user)
+        if user.profile:
+            print(user.profile)
+
+
+async def create_activity(
+    session: AsyncSession,
+    user_id: int,
+    *activies_in: str,
+) -> list[Activity]:
+    activies = [
+        Activity(
+            color=color,
+            category=category,
+            interval=timedelta(days=interval),
+            date=database.now(),
+            user_id=user_id,
+        )
+        for color, category, interval, _ in activies_in
+    ]
+
+
+async def main():
+    async with database.session_factory() as session:
+        await show_users_and_profile(session)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
