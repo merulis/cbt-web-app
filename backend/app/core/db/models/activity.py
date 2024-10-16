93580a07080620457eadc9758ae7b54f803b9ed1
merulis <merulis@yandex.ru>
2024-10-16 12:08:47 +0300

    migration

--- backend/app/core/db/models/activity.py
+++ backend/app/core/db/models/activity.py
@@ -1,13 +1,17 @@
 from datetime import datetime, timedelta
 
-from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.db import Base
 
 
 class Activity(Base):
-
     color: Mapped[str]
     type: Mapped[str]
     interval: Mapped[timedelta]
     date: Mapped[datetime]
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user.id"),
+    )
