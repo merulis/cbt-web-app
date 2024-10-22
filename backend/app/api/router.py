7170612a83a521eb00a0437f4b71b3c7f37c1116
merulis <merulis@yandex.ru>
2024-10-22 14:36:22 +0300

    add login and user in routes layer

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
 from app.api import activity
+from app.api import user
+from app.api import login
 
 api_router = APIRouter()
-api_router.include_router(activity.router, tags=["Activity tracker"])
+api_router.include_router(user.router, tags=["User"])
+api_router.include_router(login.router, tags=["Login"])
+api_router.include_router(activity.router, tags=["Activity"])
