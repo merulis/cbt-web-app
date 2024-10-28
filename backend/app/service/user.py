4c180a703eae6cb5730335b750f04fcca40b7f1d
merulis <merulis@yandex.ru>
2024-10-28 17:25:15 +0300

    remove auth logic

--- backend/app/service/user.py
+++ backend/app/service/user.py
@@ -1,41 +1,8 @@
-from datetime import timedelta
-
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db.crud import user as data
 
 from app.schemas.user import User, UserCreate, UserUpdate
-
-
-def verify_password(plain: str, hash: str) -> bool:
-    ...
-
-
-def get_hash(plain: str) -> str:
-    ...
-
-
-def get_jwt_username(token: str) -> str | None:
-    ...
-
-
-def get_current_user(token: str) -> User | None:
-    ...
-
-
-def lookup_user(id_: int) -> User | None:
-    ...
-
-
-def auth_user(id_: int, plain: str) -> User | None:
-    ...
-
-
-def create_access_token(
-    data: dict,
-    expires: timedelta | None = None,
-):
-    ...
 
 
 def get_all() -> list[User]:
