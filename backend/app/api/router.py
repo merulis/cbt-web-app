01171825c750c9ec65b7407a0a224f220ad227af
merulis <merulis@yandex.ru>
2024-10-28 17:24:29 +0300

    rename login router to auth

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from app.api import activity
 from app.api import user
-from app.api import login
+from app.api import auth
 
 api_router = APIRouter()
 api_router.include_router(user.router, tags=["User"])
-api_router.include_router(login.router, tags=["Login"])
+api_router.include_router(auth.router, tags=["Auth"])
 api_router.include_router(activity.router, tags=["Activity"])
