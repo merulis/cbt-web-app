73aecd6e27693cd536dccba5c8ba54dd2316a0fb
merulis <merulis@yandex.ru>
2024-10-28 17:24:01 +0300

    add expire logic for token

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,16 +1,47 @@
+from datetime import datetime, timedelta
+
 import jwt
 import bcrypt
 
 from app.core.settings import settings
 
 
+def encode_expire(
+    payload: dict,
+    expire_minutes: int = settings.JWT.ACCESS_TOKEN_EXPIRE_MINUTES,
+    expire_timedelta: timedelta | None = None,
+):
+    to_encode = payload.copy()
+    now = datetime.now()
+
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+
+    to_encode.update(
+        exp=expire,
+        iat=now,
+    )
+
+    return to_encode
+
+
 def encode_jwt(
     payload: dict,
     private_key: str = settings.JWT.PRIVATE_KEY.read_text(),
     algorithm: str = settings.JWT.ALGORITHM,
+    expire_minutes: int = settings.JWT.ACCESS_TOKEN_EXPIRE_MINUTES,
+    expire_timedelta: timedelta | None = None,
 ):
-    encoded = jwt.encode(
+    payload_with_expire = encode_expire(
         payload=payload,
+        expire_minutes=expire_minutes,
+        expire_timedelta=expire_timedelta,
+    )
+
+    encoded = jwt.encode(
+        payload=payload_with_expire,
         key=private_key,
         algorithm=algorithm,
     )
