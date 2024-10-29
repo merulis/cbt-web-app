bdedf9731b2c59b1c495e0f622fc81dc241d6af5
merulis <merulis@yandex.ru>
2024-10-29 21:34:12 +0300

    inplement logging with retries

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,13 @@
+import logging
+
 from contextlib import asynccontextmanager
+from tenacity import (
+    after_log,
+    before_log,
+    retry,
+    stop_after_attempt,
+    wait_fixed,
+)
 
 from fastapi import FastAPI
 
@@ -13,6 +22,13 @@ async def lifespan(app: FastAPI):
     # pre shutdown actions
 
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    filename=settings.LOG.LOG_PATH / "log_backend.log",
+    format=settings.LOG.FORMAT,
+    level=logging.INFO,
+)
+
 app = FastAPI(
     lifespan=lifespan,
     title=settings.PROJECT_NAME,
@@ -21,7 +37,21 @@ app = FastAPI(
 app.include_router(api_router)
 
 
-if __name__ == "__main__":
+@retry(
+    stop=stop_after_attempt(settings.LOG.RETRY_MAX_TRIES),
+    wait=wait_fixed(settings.LOG.RETRY_WAIT_SEC),
+    before=before_log(logger, logging.INFO),
+    after=after_log(logger, logging.WARN),
+)
+def main():
     import uvicorn
 
+    logger.info("Uvicorn run app")
+    raise Exception
     uvicorn.run(app="main:app", reload=True)
+    logger.info("Uvicorn shutdown app")
+
+
+if __name__ == "__main__":
+    for _ in range(19):
+        main()
