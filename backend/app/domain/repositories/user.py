ec2ec842b04563be6fb8ba4efb8971527aa70138
merulis <merulis@yandex.ru>
2025-01-31 20:30:59 +0300

    [update] User -> UserEntity, some return value

--- backend/app/domain/repositories/user.py
+++ backend/app/domain/repositories/user.py
@@ -2,26 +2,26 @@ from abc import ABC, abstractmethod
 
 import uuid
 
-from app.domain.entities.user import User
+from app.domain.entities.user import UserEntity
 
 
 class IUserRepository(ABC):
     @abstractmethod
-    def save(self, user: User) -> User:
+    def save(self, user: UserEntity) -> UserEntity:
         pass
 
     @abstractmethod
-    def get_by_uid(self, uid: uuid.UUID) -> User | None:
+    def get_by_uid(self, uid: uuid.UUID) -> UserEntity | None:
         pass
 
     @abstractmethod
-    def get_by_email(self, email: str) -> User | None:
+    def get_by_email(self, email: str) -> UserEntity | None:
         pass
 
     @abstractmethod
-    def update(self, user: User) -> User:
+    def update(self, user: UserEntity) -> UserEntity | None:
         pass
 
     @abstractmethod
-    def delete(self, uid: uuid.UUID) -> bool:
+    def delete(self, uid: uuid.UUID) -> None:
         pass
