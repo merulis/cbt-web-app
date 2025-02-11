af2981f2a93d9fa1c162182aa8f6a2f8bcf14cef
merulis <merulis@yandex.ru>
2025-02-11 15:38:01 +0300

    [remove] old routes [add] actual routes

--- backend/app/router.py
+++ backend/app/router.py
@@ -1,11 +1,7 @@
 from fastapi import APIRouter
-from app.apps.activity import routes as activity_routes
-from app.apps.auth import routes as auth_routes
-from app.apps.activity import routes as user_routes
+from app.presentation.routes.user_endpoints import router as user_routes
 
 
 api_router = APIRouter()
 
-api_router.include_router(user_routes.router, tags=["User"])
-api_router.include_router(auth_routes.router, tags=["Auth"])
-api_router.include_router(activity_routes.router, tags=["Activity"])
+api_router.include_router(user_routes, tags=["User"])
