d046dfcf6ba74af7b5081d01ebed8c3677f9d689
merulis <merulis@yandex.ru>
2025-02-11 15:30:21 +0300

    [update] uuid -> int [add] username in user [replace] UserEntity for passing in repo -> UserDTO

--- backend/app/application/use_cases/user_register.py
+++ backend/app/application/use_cases/user_register.py
@@ -1,8 +1,8 @@
-import uuid
-
 from app.domain.entities.user import UserEntity
 from app.domain.repositories.user import IUserRepository
+
 from app.application.interfaces.auth import IPasswordHasher
+from app.application.dto import UserDTO
 
 
 class RegisterUserCase:
@@ -10,15 +10,20 @@ class RegisterUserCase:
         self.repo = user_repo
         self.hasher = hasher
 
-    def execute(self, email: str, plain_password: str) -> UserEntity:
+    def execute(
+        self,
+        username: str,
+        email: str,
+        plain_password: str,
+    ) -> UserEntity:
         existing_user = self.repo.get_by_email(email=email)
         if existing_user is not None:
             raise ValueError("User with given email already exists")
 
         hashed_pw = self.hasher.hash_password(plain_password=plain_password)
 
-        user = UserEntity(
-            id=uuid.uuid4(),
+        user = UserDTO(
+            username=username,
             email=email,
             hashed_password=hashed_pw,
         )
